109aed7d8cec7c302a401bae8e4b9c59b11de7ad
manu98 <mocanu.emanuel.cristian@gmail.com>
2021-11-02 19:14:34 +0200

    Â

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import dao.ClientRepository;
 import util.HibernateUtil;
 
 public class Main {
@@ -5,20 +6,29 @@ public class Main {
 
         //TODO: SignIn/LogIn
         /*
-        LogIn verifica daca emailul si parola introdusa exista.
-        SignIm creaza un client/trainer nou si il introduce in baza de date.
-        Daca s-a autentificat ca si client atunci sa ii apara meniu cu functii specifice client repository:
-        -display all trainers
-        -display all courses
-        -assign to a trainer
-        -assign to a course
-        daca s-a autentificat ca si trainer sa aiba functii specifice trainer repository:
-        -sa vada toti clientii
-        -sa vada toate cursurile lui
-        -sa elimimne un client din lista
-        -sa renunte la un curs
-         */
+        LogIn verifica daca emailul si parola introdusa exista. -AZ
+        -
+        SignIn creaza un client/trainer nou si il introduce in baza de date.
+        Clientul care se va crea va avea email si parola.
 
+        Daca s-a autentificat ca si client atunci sa ii apara meniu cu functii specifice client repository:
+        -set firstname, lastname, age... -DB
+        -display all trainers - done
+        -display all courses -M
+        -assign to a trainer - setam idTrainer din client = trainer primit ca si parametru (numele unui trainer- cauta in trainers
+        numele primit ca si parametru, daca il gaseste seteaza idTrainer din tabela client = idTrainer din tabela trainer care are numele
+        = numele primit ca si parametru) - AZ
+        -assign to a course - string primit ca si parametru (ca si mai sus) -AȚ
+
+        Daca s-a autentificat ca si trainer sa aiba functii specifice trainer repository:
+                -set firstname, lastname, age... -DB
+        -sa vada toti clientii (displayAllClients()) -M
+        -sa vada toate cursurile lui (displayAllTrainersCourses()) -M
+        -sa elimimne un client din lista (seteaza idClient din tabela trainer "null") - AȚ
+        -sa renunte la un curs (seteze idCourse ca si "null" - DB
+         */
+        ClientRepository clientRepository = new ClientRepository();
+        clientRepository.displayAllTrainers().stream().forEach(System.out::println);
         HibernateUtil.shutdown();
     }
 }
--- src/main/java/dao/ClientRepository.java
+++ src/main/java/dao/ClientRepository.java
@@ -1,6 +1,26 @@
 package dao;
 
 
+import entity.Trainer;
+import org.hibernate.Session;
+import util.HibernateUtil;
+
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClientRepository {
+public List<Trainer> displayAllTrainers() {
+    List<Trainer> trainerList = new ArrayList<>();
+    try {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("select t from Trainer t", Trainer.class);
+        trainerList = query.getResultList();
+    } catch (Exception ex) {
+        ex.printStackTrace();
+        return null;
+    }
+    return trainerList;
+}
 
 }
