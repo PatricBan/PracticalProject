a76f61a7eeba629da9ed51fd54dc9489bbea7887
manu98 <mocanu.emanuel.cristian@gmail.com>
2021-11-02 20:06:11 +0200

    displays

--- src/main/java/dao/ClientRepository.java
+++ src/main/java/dao/ClientRepository.java
@@ -1,6 +1,40 @@
 package dao;
 
 
-public class ClientRepository {
+import entity.Course;
+import entity.Trainer;
+import org.hibernate.Session;
+import util.HibernateUtil;
 
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientRepository {
+    public List<Trainer> displayAllTrainers() {
+        List<Trainer> trainerList = new ArrayList<>();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery("select t from Trainer t", Trainer.class);
+            trainerList = query.getResultList();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return trainerList;
+    }
+
+    public List<Course> displayAllCourses() {
+        List<Course> courseList = new ArrayList<>();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery("select c from Course c", Course.class);
+            courseList = query.getResultList();
+        } catch (
+                Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return courseList;
+    }
 }
--- src/main/java/dao/TrainerRepository.java
+++ src/main/java/dao/TrainerRepository.java
@@ -1,5 +1,39 @@
 package dao;
 
+import entity.Client;
+import entity.Course;
+import org.hibernate.Session;
+import util.HibernateUtil;
+
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainerRepository {
+    public List<Client> displayAllClients(){
+        List<Client> clientList = new ArrayList<>();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery("select c from Client c", Client.class);
+            clientList = query.getResultList();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return clientList;
+    }
+    public List<Course> displayAllTrainerCourses() {
+        List<Course> courseList = new ArrayList<>();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery("select c from Course c", Course.class);
+            courseList = query.getResultList();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return courseList;
+    }
+
 
 }
